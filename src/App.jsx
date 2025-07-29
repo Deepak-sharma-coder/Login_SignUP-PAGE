4b0ec3ca165eac466591dd0539a01e24aa8e10d9
Deepak Sharma <deepaksharma9102003@gmail.com>
2025-07-29 14:05:36 +0530

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+import LoginSignUP from './Components/LoginSingUP/LoginSingUP';
+
+function App() {
+  return (
+    <>
+   <LoginSignUP/>
+    </>
+  )
+}
+
+export default App
---
+++ src/assets/assets.js
@@ -0,0 +1,10 @@
+import email_icon from './email.png'
+import user_icon from './person.png'
+import password_icon from './password.png'
+
+
+export const assets = {
+    email_icon,
+    user_icon,
+    password_icon
+}
